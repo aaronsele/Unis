866a94c9fba4af884cebed2a1d0589c8d1894ebb
aaronsele <48522418@est.ort.edu.ar>
2025-07-02 21:01:18 -0300

    robomendez

--- Unis-Proyecto/src/components/VocationalGuidance.jsx
+++ Unis-Proyecto/src/components/VocationalGuidance.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import '@splinetool/viewer';
+import './VocationalGuidance.css'
 
 export function VocationalGuidance() {
   return (
     <div>
       <h1>aca va el OVO</h1>
+      <div className='roboAmigo'><spline-viewer url="https://prod.spline.design/B8O-mb1DKzVohPvS/scene.splinecode"></spline-viewer></div>
+
     </div>
   );
 }
